3821bdc0d12e3844fc247c56fa80de3cf34705d8
spodila07 <136508386+spodila07@users.noreply.github.com>
2023-08-03 19:44:46 -0400

    Update modelTrainMultipleNode.sh

--- MultipleNode/modelTrainMultipleNode.sh
+++ MultipleNode/modelTrainMultipleNode.sh
@@ -1,17 +1,19 @@
-#!/bin/bash
-#SBATCH --job-name=dask-worker
-#SBATCH --nodes=5                # Specify the number of nodes
-#SBATCH --ntasks=5               # Allocate 5 tasks/processes for the job (one for each node)
-#SBATCH --cpus-per-task=2        # Assign 3 CPUs for each task
-#SBATCH --mem-per-cpu=1G         # Allocate 2GB of memory for each CPU
-#SBATCH --time=01:00:00          # Set a time limit of 1 hour for the job
-#SBATCH --output=dask-worker.o%j # Specify the file to save job's standard output. %j is replaced by the job ID
-#SBATCH --partition=hpg-default  # Specify the partition/queue where the job will be submitted
-#SBATCH --qos=uf-iccc-b
-module purge                     # Removes all currently loaded modules to ensure a clean environment
-module load python/3.10          # Loads the Python version 3.10 module
-python3 -m venv env              # Creates a Python virtual environment in a directory "env"
-source env/bin/activate          # Activates the created virtual environment
-pip install dask distributed scikit-learn joblib dask-jobqueue pandas # Uses pip to install the required Python packages in the virtual environment
-python ex.py                     # Runs the Python script named "ex.py"
+#!/bin/bash                                                     # Use the Bash shell to interpret the script
+#SBATCH --job-name=modelTrainMultipleNodesJob                   # Set job name to "ex3"
+#SBATCH --nodes=3                                               # Request 3 nodes
+#SBATCH --ntasks-per-node=1                                     # Run one task per node
+#SBATCH --mem-per-cpu=1GB                                       # Allocate 1GB of memory per CPU
+#SBATCH --time=00:30:00                                         # Limit job run time to 30 minutes
+#SBATCH --partition=hpg-default                                 # Submit job to the "hpg-default" partition
+#SBATCH --output=modelTrainMultipleNodes_output.log             # Send output to "ex3_output.log" file
+#SBATCH --qos=uf-iccc-b                                         # Assign Quality of Service (QoS) level
 
+module purge                                                    # Remove all loaded modules to start with a clean slate
+module load python/3.10                                         # Load Python 3.10
+
+python3 -m venv /blue/uf-iccc/spodila/myenv  # Create virtual environment
+source /blue/uf-iccc/spodila/myenv/bin/activate # Activate virtual environment
+
+pip install dask distributed scikit-learn joblib dask-jobqueue pandas # Install required Python packages
+
+python modelTrainMultipleNode.py                                # Run Python script for model training
